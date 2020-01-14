b5fad0a71b705900ae6cab19f1ebde2c24e8ffbf
Дмитрий <dima-tkachenko@mail.ru>
2020-01-14 18:26:30 +0300

    Создана модель пользователя

---
+++ Fitness.BL/Model/User.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    public class User
+    {
+        #region Свойства
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Пол
+        /// </summary>
+        public Gender Gender { get; }
+        /// <summary>
+        /// Дата рождения
+        /// </summary>
+        public DateTime BirthDate { get; }
+        /// <summary>
+        /// Вес
+        /// </summary>
+        public double Weight { get; set; }
+        /// <summary>
+        /// Рост
+        /// </summary>
+        public double Height { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Создать нового пользователя
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="gender">Пол</param>
+        /// <param name="birthDate">Дата рождения</param>
+        /// <param name="weight">Вес</param>
+        /// <param name="height">Рост</param>
+        public User(string name,
+                    Gender gender,
+                    DateTime birthDate,
+                    double weight,
+                    double height)
+        {
+            #region Проверка условий
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Поле пользователя не может быть пустым или null", nameof(name));
+            }
+
+            if (Gender == null)
+            {
+                throw new ArgumentNullException("Пол не может быть null", nameof(gender));
+
+            }
+
+            if (birthDate < DateTime.Parse("01.01.1900") && birthDate >= DateTime.Now)
+            {
+                throw new ArgumentException("Не возможная дата рождения", nameof(birthDate));
+            }
+
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Вес не может быть меньше или равен нулю", nameof(weight));
+            }
+
+            if (height <= 0)
+            {
+                throw new ArgumentException("Рост не может быть меньше или равен нулю", nameof(height));
+            }
+            #endregion
+
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Fitness.CMD/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Fitness.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
